36532f024c2c003d9c8f50378c58a2a436af2697
Amber Wilson <amberwilson@MacBook-Pro.local>
2023-01-17 13:26:21 -0600

    added navbar with paths to search athlete, login, signup modal, and home. Also added bootstrap, bootstrap-react

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-import './home.css';
+
 
 const LoginForm = () => {
+  
   return <div>LoginForm
     Test-please login!
   </div>;
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import './home.css';
+// import './home.css';
 
 const SignUp = () => {
   return (
