9c540dfcb60a22cbbdaed23d26dd21530022625c
Nerakma84 <fnk8476@gmail.com>
2023-01-17 03:04:39 -0600

    rendered components to page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import SignUp from './components/Home/SignUp';
 import Athlete from './components/Athlete/Athlete';
 import Dashboard from './components/Dashboard/Dashboard';
 import Wods from './components/Wods/Wods';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+
 const httpLink = createHttpLink({
   uri: '/graphql',
   cache: new InMemoryCache(),
@@ -15,22 +17,25 @@ const httpLink = createHttpLink({
 //login auth
 const client = new ApolloClient({
   link: httpLink,
+  cache: new InMemoryCache(),
 })
 
 function App() {
   return (
     <ApolloProvider client = { client }>
     <Router>
-      <>
+      <div className="align-center title">
         <Navbar />
-        <Routes>
-          <Route exact path='/' component={LoginForm} />
-          <Route exact path='/signup' component={SignUp} />
-          <Route exact path='/athlete' component={Athlete} />
-          <Route exact path='/dashboard' component={Dashboard} />
-          <Route exact path='/wods' component={Wods} />
-        </Routes>
-    </>
+        <div className='container home'>
+          <LoginForm />
+          <SignUp />
+        </div>
+        <div>
+          <Athlete />
+          <Dashboard />
+          <Wods />
+        </div>
+    </div>
     </Router>
     </ApolloProvider>
   );
--- client/src/components/Home/LoginForm.jsx
+++ client/src/components/Home/LoginForm.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import './home.css';
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return <div>LoginForm
+    Test-please login!
+  </div>;
 };
 
 export default LoginForm;
