1a8be68775ce315c6ffd29e2893d4e4e06acc747
Your Name <you@example.com>
2023-01-18 14:06:06 -0600

    Creating form for Updating an Athlete, used bootstrap, Also added Route in App.jsx

--- client/src/components/Athlete/Update.jsx
+++ client/src/components/Athlete/Update.jsx
@@ -1,11 +1,41 @@
+//???? We need to think through images.....
+
 import React from 'react';
 
 import './athlete.css';
 
-const Update = () => {
+import Form from 'react-bootstrap/Form';
+
+function TextControlsExample() {
   return (
-    <div>Update</div>
-  )
+    <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="name" placeholder="Athlete First Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="name" placeholder="Athlete Last Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control type="email" placeholder="athlete@athlete.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Phone Number</Form.Label>
+        <Form.Control type="number" placeholder="(000)000-0000" />
+      </Form.Group>
+      {/* Think through images.... */}
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Notes</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Injury Report</Form.Label>
+        <Form.Control as="textarea" rows={1} />
+      </Form.Group>
+    </Form>
+  );
 }
 
-export default Update
+export default TextControlsExample;
