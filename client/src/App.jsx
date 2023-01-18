9c3a9f8cfe633d943b9c169cdf35a1692fc59947
tierney03 <tierney_03@mac.com>
2023-01-17 21:27:01 -0600

    making things pretty

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,39 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Home from './pages/Home/home.jsx';
-import LoginForm from './components/LoginForm.jsx';
-import SignUp from './components/SignUp';
-import Athlete from './components/Athlete/Athlete';
-import NewAthlete from './components/NewAthlete/NewAthlete';
+import Home from "./pages/Home/home.jsx";
+import LoginForm from "./components/LoginForm.jsx";
+import SignUp from "./components/SignUp";
+import Athlete from "./components/Athlete/Athlete";
+import NewAthlete from "./components/NewAthlete/NewAthlete";
 
-import Dashboard from './components/Dashboard/Dashboard';
-import Wods from './components/Wods/Wods';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import Dashboard from "./components/Dashboard/Dashboard";
+import Wods from "./components/Wods/Wods";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 
 import Auth from "./utils/auth";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 //login auth
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-})
+});
 
 function App() {
   return (
-    <ApolloProvider client = { client }>
-    {/* <Router>
+    <ApolloProvider client={client}>
+      {/* <Router>
       <div className="align-center title">
         <Navbar />
         <div className='container home'>
@@ -42,14 +47,19 @@ function App() {
         </div>
     </div>
     </Router> */}
-    <Router>
+      <Router>
         <>
           <Navbar />
           <Routes>
-            <Route exact path='/' element={(Auth.loggedIn() ? < Dashboard /> : < Home /> )} />
-            <Route exact path='/search' element={< Athlete />} />
-            <Route exact path='/add' element={< NewAthlete />} />
-            <Route element={() => <h1 className='display-2'>Wrong page!</h1>} />
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route
+              exact
+              path="/"
+              element={Auth.loggedIn() ? <Dashboard /> : <Home />}
+            />
+            <Route exact path="/search" element={<Athlete />} />
+            <Route exact path="/add" element={<NewAthlete />} />
+            <Route element={() => <h1 className="display-2">Wrong page!</h1>} />
           </Routes>
         </>
       </Router>
---
+++ client/src/components/NewAthlete/NewAthlete.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "./NewAthlete.css";
+
+export default function NewAthlete() {
+  return (
+    <Card sx={{ maxWidth: 500 }} className="card">
+      <CardMedia
+        sx={{ height: 200 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Edit</Button>
+        <Button size="small">Delete</Button>
+      </CardActions>
+    </Card>
+  );
+}
