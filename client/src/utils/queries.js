b6fb1b595fa0213863a3294a619996c58f7e939e
tierney03 <tierney_03@mac.com>
2023-01-19 10:59:33 -0600

    rogue curly brackets

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -8,33 +8,32 @@ export const QUERY_ME = gql`
       email
       currentAthletes {
         _id
-        firstName 
-        lastName 
-        email 
-        phoneNumber 
-        image 
-        notes 
-        injuryReport 
-        wods 
+        firstName
+        lastName
+        email
+        phoneNumber
+        image
+        notes
+        injuryReport
+        wods
       }
     }
   }
 `;
 
-// ????? 
+// ?????
 export const GET_ATHLETES = gql`
   query getAthletes {
     athletes {
-        _id
-        firstName 
-        lastName 
-        email 
-        phoneNumber 
-        image 
-        notes 
-        injuryReport 
-        wods 
-      }
+      _id
+      firstName
+      lastName
+      email
+      phoneNumber
+      image
+      notes
+      injuryReport
+      wods
     }
   }
 `;
@@ -43,16 +42,15 @@ export const GET_ATHLETES = gql`
 export const GET_ATHLETE = gql`
   query getAthlete($_id: ID!) {
     athlete(_id: $_id) {
-        _id
-        firstName 
-        lastName 
-        email 
-        phoneNumber 
-        image 
-        notes 
-        injuryReport 
-        wods 
-      }
+      _id
+      firstName
+      lastName
+      email
+      phoneNumber
+      image
+      notes
+      injuryReport
+      wods
     }
   }
 `;
